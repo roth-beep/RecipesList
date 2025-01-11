0731b33b125036b57745ffc20b4f41c6b452188a
sahil <sahilpreddy96@gmail.com>
2025-01-11 16:39:22 -0700

    Initial Commit

---
+++ RecipesList/RecipesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesListApp.swift
+//  RecipesList
+//
+//  Created by Sahil Reddy on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesListTests/RecipesListTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesListTests.swift
+//  RecipesListTests
+//
+//  Created by Sahil Reddy on 1/11/25.
+//
+
+import Testing
+@testable import RecipesList
+
+struct RecipesListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
